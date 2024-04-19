b5ad1ac4fb2b0dd64df1af40724e3c9e32398fdc
Natael Bernardo <natael.bernardo@hotmail.com>
2024-04-19 14:24:00 +0200

    feat: create resource-settings package

---
+++ packages/resource-settings/README.md
@@ -0,0 +1,11 @@
+# resource-settings
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build resource-settings` to build the library.
+
+## Running unit tests
+
+Run `nx test resource-settings` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ packages/resource-settings/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'resource-settings',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/resource-settings',
+};
